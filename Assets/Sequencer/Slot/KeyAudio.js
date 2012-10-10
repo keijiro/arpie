cae35f7bc267f4102412d1d8f5fa8d200fb07464
keijiro <keijiro@gmail.com>
2012-10-11 08:12:12 +0900

    Added several musical scales.

--- Assets/Sequencer/Slot/KeyAudio.js
+++ Assets/Sequencer/Slot/KeyAudio.js
@@ -1,8 +1,17 @@
 #pragma strict
 
-static var baseNote = 53;
-static var intervals = [0, 2, 4, 7, 9];
+static var baseNote = 60 - 12;
 static var clipLength = 0.3;
+
+//static var intervals = [0, 2, 4, 7, 9];			// Pentatonic
+//static var intervals = [0, 4, 5, 7, 11];			// Ryukyu
+//static var intervals = [0, 2, 4, 6, 8, 10];		// Whole tone
+//static var intervals = [0, 1, 4, 5, 7, 8, 10];		// Phrygian dominant
+//static var intervals = [0, 1, 4, 5, 7, 8, 11];		// Double harmonic (Arabic)
+//static var intervals = [0, 2, 4, 5, 6, 8, 10];		// Major Locrian (Arabic)
+//static var intervals = [0, 2, 3, 6, 7, 8, 11];		// Hungarian minor
+//static var intervals = [0, 2, 3, 4, 7, 9];		// Major blues
+static var intervals = [0, 2, 4, 5, 7, 9, 11];	// Diatonic
 
 var degree = 0;
 
@@ -11,6 +20,7 @@ function Start() {
 	var env = Envelope();
 	var bit = Bitcrusher();
 
+	degree += intervals.Length - 2;
 	osc.SetNote(baseNote + intervals[degree % intervals.Length] + degree / intervals.Length * 12);
 	env.KeyOn();
 
@@ -24,5 +34,5 @@ function Start() {
 }
 
 function KeyOn() {
-	audio.Play();
+	audio.PlayOneShot(audio.clip);
 }
