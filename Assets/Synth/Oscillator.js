f3bbcecd833852eafc5a177375c623c7a23b4d67
keijiro <keijiro@gmail.com>
2012-10-11 22:59:01 +0900

    Removed frequency modulation.

--- Assets/Synth/Oscillator.js
+++ Assets/Synth/Oscillator.js
@@ -1,24 +1,10 @@
 #pragma strict
 
 class Oscillator {
-	var multiplier = 4.0;
-	var modulation = 0.0;
-
-	private var mx = 0.0;
 	private var cx = 0.0;
 	private var step = 0.0;
 
-	static private var kPi = 3.14159265359;
 	static private var kPi2 = 6.28318530718;
-	static private var k4dPi = 1.27323954474;
-	static private var k4dPiPi = 0.40528473456;
-
-	// http://devmaster.net/forums/topic/4648-fast-and-accurate-sinecosine/
-	static private function fast_sin(x : float) {
-		x -= kPi;
-		x = k4dPi * x - k4dPiPi * x * Mathf.Abs(x);
-        return -0.225 * (x * Mathf.Abs(x) - x) - x;
-	}
 	
 	function SetNote(note : int) {
 		var freq = 440.0 * Mathf.Pow(2.0, 1.0 * (note - 69) / 12.0);
@@ -26,12 +12,8 @@ class Oscillator {
 	}
 	
 	function Run() {
-		mx += step * multiplier;
 		cx += step;
-		mx -= Mathf.Floor(mx);
 		cx -= Mathf.Floor(cx);
-		var x = cx + modulation * fast_sin(kPi2 * mx);
-		x -= Mathf.Floor(x);
-		return fast_sin(kPi2 * x);
+		return Mathf.Sin(kPi2 * cx);
 	}
 }
