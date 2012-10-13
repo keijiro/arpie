1a046ff443047fac0a15aebbce0d1205eb25da75
keijiro <keijiro@gmail.com>
2012-10-13 16:03:42 +0900

    Added quantization.

--- Assets/Sequencer/Arpie/ArpieMovement.js
+++ Assets/Sequencer/Arpie/ArpieMovement.js
@@ -1,30 +1,34 @@
 #pragma strict
 
+var stepPerMin = 200.0;
 var interval = 1.0;
+var quantization = 0.8;
 
-private var time = 0.0;
+private var phase = 0.0;
 private var step = 0;
 
-private var running = false;
+private var delaying = true;
 
 function Start() {
-	var step = Mathf.FloorToInt(10.0 * Time.time / Mathf.PI);
-	while (step == Mathf.FloorToInt(10.0 * Time.time / Mathf.PI)) yield;
-	running = true;
+	// Quantize the current time.
+	var waitFor = 60.0 * (Mathf.FloorToInt(stepPerMin * Time.time / 60.0) + 1) / stepPerMin;
+	// Apply the quantization ratio.
+	waitFor = Mathf.Lerp(Time.time, waitFor, quantization);
+	// Wait for the timing.
+	while (Time.time < waitFor) yield;
+	// Start!
+	delaying = false;
 }
 
 function Update() {
-	if (!running) return;
+	if (delaying) return;
 
-	time += Time.deltaTime;
+	phase += stepPerMin * Time.deltaTime / (60.0 * interval);
+	transform.localPosition.y = interval * Mathf.Abs(Mathf.Cos(phase * Mathf.PI));
 
-	var param = 10.0 * time / interval;
-	var currentStep = Mathf.FloorToInt((param + 0.5 * Mathf.PI) / Mathf.PI);
-
-	transform.localPosition.y = interval * Mathf.Abs(Mathf.Cos(param));
-
-	if (currentStep > step) {
+	var stepNew = Mathf.FloorToInt(phase + 0.5);
+	if (stepNew > step) {
 		transform.parent.gameObject.BroadcastMessage("KeyOn");
-		step = currentStep;
+		step = stepNew;
 	}
 }
