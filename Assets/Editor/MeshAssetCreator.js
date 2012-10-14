cc53429ebc3a0618fe5c2e155f8677843fd3bb45
keijiro <keijiro@gmail.com>
2012-10-14 13:49:24 +0900

    Refined GUI parts.

---
+++ Assets/Editor/MeshAssetCreator.js
@@ -0,0 +1,29 @@
+#pragma strict
+
+@MenuItem("Custom/Assets/Create Mesh (Quad)")
+static function CreateQuadMesh() {
+    var mesh = Mesh();
+    mesh.vertices = [
+        Vector3(-1, -1, 0),
+        Vector3(-1,  1, 0),
+        Vector3( 1, -1, 0),
+        Vector3( 1,  1, 0)
+    ];
+    mesh.normals = [
+        Vector3(0, 0,  1),
+        Vector3(0, 0,  1),
+        Vector3(0, 0,  1),
+        Vector3(0, 0,  1)
+    ];
+    mesh.uv = [
+        Vector2(0, 0),
+        Vector2(0, 1),
+        Vector2(1, 0),
+        Vector2(1, 1)
+    ];
+    mesh.SetIndices([0, 1, 2, 3], MeshTopology.TriangleStrip, 0);
+    mesh.RecalculateBounds();
+
+    AssetDatabase.CreateAsset(mesh, "Assets/Quad.asset");
+    AssetDatabase.Refresh();
+}
