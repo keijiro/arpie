ae3b7ead32e8cd4e07462f3d108867fcc5b1f918
keijiro <keijiro@gmail.com>
2012-10-14 14:11:54 +0900

    Refined the notes.

--- Assets/GUI/NoteController.js
+++ Assets/GUI/NoteController.js
@@ -1,5 +1,7 @@
 #pragma strict
 
+var alpha = 0.8;
+
 private var sx = 1.0;
 private var sy = 1.0;
 private var timer = 0.0;
@@ -8,15 +10,16 @@ function Start() {
 	sx = Random.Range(4.0, 6.0);
 	sy = Random.Range(4.0, 6.0);
 	timer = 10.0;
+	renderer.material.color.a = alpha;
 }
 
 function Update() {
 	timer -= Time.deltaTime;
 	if (timer > 0.0) {
-		var dx = 0.1 * Mathf.Sin(Time.time * sx);
-		var dy = 0.1 * Mathf.Sin(Time.time * sy);
+		var dx = 0.05 * Mathf.Sin(Time.time * sx);
+		var dy = 0.05 * Mathf.Sin(Time.time * sy);
 		transform.localPosition = Vector3(dx, dy, 0.0);
-		if (timer < 1.0) renderer.material.color.a = timer;
+		if (timer < 1.0) renderer.material.color.a = alpha * timer;
 	} else {
 		Destroy(gameObject);
 	}
