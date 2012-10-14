a827752a47af8d0e35cc9930a262e0b6304ec7a8
keijiro <keijiro@gmail.com>
2012-10-14 15:02:06 +0900

    Refined color palette.

--- Assets/Sequencer/Arpie/ArpieColorChanger.js
+++ Assets/Sequencer/Arpie/ArpieColorChanger.js
@@ -1,13 +1,6 @@
 #pragma strict
 
-private static var colors = [
-	Color(0.5, 0.5, 1.0),
-	Color(0.5, 1.0, 0.5),
-	Color(0.5, 1.0, 1.0),
-	Color(1.0, 0.5, 0.5),
-	Color(1.0, 0.5, 1.0),
-	Color(1.0, 1.0, 0.5)
-];
+var colors : Color[];
 
 static var counter = 0;
 
