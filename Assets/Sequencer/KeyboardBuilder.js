2a69d085ec781da6b46816b44cd7affda40cc166
keijiro <keijiro@gmail.com>
2012-10-14 21:38:16 +0900

    Tweaked audio settings.

--- Assets/Sequencer/KeyboardBuilder.js
+++ Assets/Sequencer/KeyboardBuilder.js
@@ -1,15 +1,22 @@
 #pragma strict
 
-var numberOfKeys = 6;
+var numberOfKeys = 16;
+var panning = 1.4;
+var volume = 0.35;
+var highDecay = 0.08;
 var slotPrefab : GameObject;
 
 function Start() {
 	for (var i = 0; i < numberOfKeys; i++) {
 		var position = transform.position + Vector3.right * i;
+
 		var slot = Instantiate(slotPrefab, position, Quaternion.identity) as GameObject;
 		slot.GetComponentInChildren.<KeyCube>().SetColor(0, i);
 		slot.GetComponentInChildren.<KeyAudio>().SetKey(0, i);
-		slot.GetComponentInChildren.<AudioSource>().pan = 2.0 * i / numberOfKeys - 1.0;
+		
+		var source = slot.GetComponentInChildren.<AudioSource>();
+		source.pan = panning * (i / numberOfKeys - 0.5);
+		source.volume = volume - highDecay * i / numberOfKeys;
 	}
 	Destroy(gameObject);
 }
