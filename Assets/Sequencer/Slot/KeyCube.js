0274fc58ab69f8ed6ec68860315a7c6ee5f8ab35
keijiro <keijiro@gmail.com>
2012-10-14 15:45:06 +0900

    Changed to fix the scale of the colliders.

--- Assets/Sequencer/Slot/KeyCube.js
+++ Assets/Sequencer/Slot/KeyCube.js
@@ -2,7 +2,7 @@
 
 private var vibe = 0.0;
 private var spin = 0.0;
-private var initialScale = 1.0;
+private var mesh : Transform;
 
 static private var baseColors = [
 	Color(1.0, 1.0, 1.0),		// Pentatonic
@@ -12,18 +12,18 @@ static private var baseColors = [
 	Color(1.0, 1.0, 0.7)		// Ryukyu
 ];
 
-function Start() {
-	initialScale = transform.localScale.x;
+function Awake() {
+	mesh = transform.Find("Mesh");
 }
 
 function SetColor(scaleIndex : int, degree : int) {
 	var brightness = (degree & 1) ? 0.7 : 0.9;
-	renderer.material.color = baseColors[scaleIndex % baseColors.Length] * brightness;
+	mesh.renderer.material.color = baseColors[scaleIndex % baseColors.Length] * brightness;
 }
 
 function Update() {
-	transform.localRotation = Quaternion.AngleAxis(spin * 360.0, Vector3.up);
-	transform.localScale = Vector3.one * (initialScale * (1.0 + 0.2 * vibe * Mathf.Sin(40.0 * Time.time)) * (1.0 - 0.5 * spin));
+	mesh.localRotation = Quaternion.AngleAxis(spin * 360.0, Vector3.up);
+	mesh.localScale = Vector3.one * (1.0 + 0.2 * vibe * Mathf.Sin(40.0 * Time.time)) * (1.0 - 0.5 * spin);
 	vibe = ExpEase.Out(vibe, 0.0, -8.0);
 	spin = ExpEase.Out(spin, 0.0, -8.0);
 }
