50df25f878d3b5c93b67560e36e8060995bb3e9d
Keijiro Takahashi <keijiro@gmail.com>
2020-10-07 22:30:15 +0900

    Remove unused script

--- Assets/Editor/MeshAssetCreator.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-[System.Serializable]
-public partial class MeshAssetCreator : MonoBehaviour
-{
-}// Removed temporary for the removal of MeshTopology.TriangleStrip on Unity 4.0 RC1
